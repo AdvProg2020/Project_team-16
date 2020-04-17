d1a0e29dc876b24beabe29c8aed3964943d8fb79
sajad-paksima <59170401+sajad-paksima@users.noreply.github.com>
2020-04-18 01:45:13 +0430

    create Comment Class

---
+++ src/main/java/Comment.java
@@ -0,0 +1,8 @@
+public class Comment {
+    private String productId;
+    private String userId;
+    private String title;
+    private String text;
+    private CommentStatus status;
+    private boolean boughtThisProduct;
+}
