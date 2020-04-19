738fe463bb44f7d83b63c7bb60bc8d4ae351bd44
sajad-paksima <59170401+sajad-paksima@users.noreply.github.com>
2020-04-19 17:16:43 +0430

    Update Company.java

--- src/main/java/ModelPackage/Product/Company.java
+++ src/main/java/ModelPackage/Product/Company.java
@@ -11,4 +11,11 @@ public class Company {
     private String phone;
     private String group;
     private ArrayList<Product> productsIn;
+
+    public Company(String name, String phone, String group) {
+        this.name = name;
+        this.phone = phone;
+        this.group = group;
+        this.productsIn = new ArrayList<Product>();
+    }
 }
