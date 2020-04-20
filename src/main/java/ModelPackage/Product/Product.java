893d130185b65587e753a0d53f1ca2600d720d6c
sajad-paksima <59170401+sajad-paksima@users.noreply.github.com>
2020-04-20 10:59:00 +0430

    temporary cunstructor created

--- src/main/java/ModelPackage/Product/Product.java
+++ src/main/java/ModelPackage/Product/Product.java
@@ -28,4 +28,25 @@ public class Product {
     private HashMap<String,Integer> prices;
     private int view;
     private int boughtAmount;
+                                    /*should be deleted before merge*/
+    public Product(String productId, ProductStatus productStatus, String name, Date dateAdded, Company company, ArrayList<Seller> allSellers, Category category, String categoryId, HashMap<String, String> publicFeatures, HashMap<String, String> specialFeatures, String description, ArrayList<Score> allScores, float totalScore, ArrayList<Comment> allComments, HashMap<String, Integer> stock, HashMap<String, Integer> prices, int view, int boughtAmount) {
+        this.productId = productId;
+        this.productStatus = productStatus;
+        this.name = name;
+        this.dateAdded = dateAdded;
+        this.company = company;
+        this.allSellers = allSellers;
+        this.category = category;
+        this.categoryId = categoryId;
+        this.publicFeatures = publicFeatures;
+        this.specialFeatures = specialFeatures;
+        this.description = description;
+        this.allScores = allScores;
+        this.totalScore = totalScore;
+        this.allComments = allComments;
+        this.stock = stock;
+        this.prices = prices;
+        this.view = view;
+        this.boughtAmount = boughtAmount;
+    }
 }
