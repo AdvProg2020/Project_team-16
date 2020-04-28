2b5203b480c81c31b843a9f8a830a0809ebc5e3d
ckoorosh <53394330+ckoorosh@users.noreply.github.com>
2020-04-28 18:30:45 +0430

    ManagerProfile test method added

--- src/test/java/ModelPackage/System/ManagerManagerTest.java
+++ src/test/java/ModelPackage/System/ManagerManagerTest.java
@@ -1,5 +1,45 @@
 package ModelPackage.System;
 
+import ModelPackage.Users.Cart;
+import ModelPackage.Users.Manager;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 public class ManagerManagerTest {
+    ManagerManager managerManager;
+    List<Manager> managers;
+    Manager hatam;
+
+    {
+        managerManager = ManagerManager.getInstance();
+        managers = managerManager.getManagers();
+
+        hatam = new Manager(
+                "hatam008",
+                "hatam008@kimi",
+                "Ali",
+                "Hatami",
+                "hatam008@yahoo.com",
+                "09121351223",
+                new Cart()
+        );
+    }
+
+    @Test
+    public void createManagerProfile(){
+        String[] info = {
+                "hatam008",
+                "hatam008@kimi",
+                "Ali",
+                "Hatami",
+                "hatam008@yahoo.com",
+                "09121351223"
+        };
+        managerManager.createManagerProfile(info);
+        Manager actual = managers.get(0);
+        Assert.assertEquals(hatam,actual);
+    }
 
 }
