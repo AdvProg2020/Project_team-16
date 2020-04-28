d344e4b10490031351ace066eda8924c6772cd2d
ckoorosh <53394330+ckoorosh@users.noreply.github.com>
2020-04-28 18:43:05 +0430

    Delete user test added

--- src/test/java/ModelPackage/System/ManagerManagerTest.java
+++ src/test/java/ModelPackage/System/ManagerManagerTest.java
@@ -2,15 +2,18 @@ package ModelPackage.System;
 
 import ModelPackage.Users.Cart;
 import ModelPackage.Users.Manager;
+import ModelPackage.Users.User;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ManagerManagerTest {
     ManagerManager managerManager;
     List<Manager> managers;
     Manager hatam;
+    User marmof;
 
     {
         managerManager = ManagerManager.getInstance();
@@ -25,6 +28,17 @@ public class ManagerManagerTest {
                 "09121351223",
                 new Cart()
         );
+
+        marmof = new User("marmofayezi",
+                "marmof.ir",
+                "Mohamadreza",
+                "Mofayezi",
+                "marmof@gmail.com",
+                "09121232222",
+                new Cart()
+        );
+
+        AccountManager.getInstance().getUsers().add(marmof);
     }
 
     @Test
@@ -40,6 +54,14 @@ public class ManagerManagerTest {
         managerManager.createManagerProfile(info);
         Manager actual = managers.get(0);
         Assert.assertEquals(hatam,actual);
+    }
+
+    @Test
+    public void deleteUser(){
+        managerManager.deleteUser("marmofayezi");
+        ArrayList<User> expected = new ArrayList<>();
+        ArrayList<User> actual = (ArrayList<User>) AccountManager.getInstance().getUsers();
+        Assert.assertArrayEquals(new ArrayList[]{expected}, new ArrayList[]{actual});
     }
 
 }
