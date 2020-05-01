84b836abbab13c7f78d775419144881cf1382498
ckoorosh <53394330+ckoorosh@users.noreply.github.com>
2020-05-01 13:00:44 +0430

    view sales history test implemented

--- src/test/java/ModelPackage/System/SellerManagerTest.java
+++ src/test/java/ModelPackage/System/SellerManagerTest.java
@@ -1,27 +1,37 @@
 package ModelPackage.System;
 
+import ModelPackage.Log.DeliveryStatus;
+import ModelPackage.Log.SellLog;
 import ModelPackage.Product.Company;
-import ModelPackage.System.exeption.account.UserNotAvailableException;
+import ModelPackage.Product.Product;
 import ModelPackage.Users.Cart;
 import ModelPackage.Users.Seller;
 import ModelPackage.Users.User;
-import mockit.Expectations;
-import mockit.Injectable;
 import mockit.Mock;
 import mockit.MockUp;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class SellerManagerTest {
     SellerManager sellerManager;
     Seller marmof;
+    User hatam;
     Company adidas;
+    SellLog dullSellLog;
+    SellLog skirtSellLog;
+    Product dullForKimmi;
+    Product skirtForKimmi;
+    List<SellLog> sellLogs;
 
     {
         adidas = new Company("Adidas","115", "Clothing",new ArrayList<>());
+
         sellerManager = SellerManager.getInstance();
+
         marmof = new Seller(
                 "marmofayezi",
                 "marmof.ir",
@@ -33,7 +43,41 @@ public class SellerManagerTest {
                 adidas,
                 10000000
         );
+        hatam = new User(
+                "hatam008",
+                "hatapass",
+                "Ali",
+                "Hatami",
+                "hatam008@gmail.com",
+                "+98 992 1122",
+                new Cart()
+        );
 
+        dullForKimmi = new Product();
+        skirtForKimmi = new Product();
+
+        dullSellLog = new SellLog(
+                dullForKimmi,
+                100000,
+                5,
+                hatam,
+                new Date(),
+                DeliveryStatus.DELIVERED
+        );
+        skirtSellLog = new SellLog(
+                skirtForKimmi,
+                300000,
+                7,
+                hatam,
+                new Date(),
+                DeliveryStatus.DELIVERED
+        );
+
+        sellLogs = new ArrayList<>();
+        sellLogs.add(dullSellLog);
+        sellLogs.add(skirtSellLog);
+
+        marmof.setSellLogs(sellLogs);
     }
 
 
@@ -46,7 +90,22 @@ public class SellerManagerTest {
             }
         };
 
-        Company actual = sellerManager.viewCompanyInformation("marmofayezi");
-        Assert.assertEquals(adidas,actual);
+        Company actualCompany = sellerManager.viewCompanyInformation("marmofayezi");
+        Assert.assertEquals(adidas,actualCompany);
     }
+
+    @Test
+    public void viewSalesHistory(){
+        new MockUp<AccountManager>(){
+            @Mock
+            User getUserByUsername(String username){
+                return marmof;
+            }
+        };
+
+        Assert.assertArrayEquals(sellLogs.toArray(),marmof.getSellLogs().toArray());
+    }
+
+
+
 }
