861a76c8442d2f1affa1c219b0dd685d005b2ab0
ckoorosh <53394330+ckoorosh@users.noreply.github.com>
2020-05-01 10:05:09 +0430

    View company info added

--- src/main/java/ModelPackage/System/SellerManager.java
+++ src/main/java/ModelPackage/System/SellerManager.java
@@ -1,6 +1,11 @@
 package ModelPackage.System;
 
 
+import ModelPackage.Product.Company;
+import ModelPackage.System.exeption.account.UserNotAvailableException;
+import ModelPackage.Users.Seller;
+import ModelPackage.Users.User;
+
 public class SellerManager {
     private static SellerManager sellerManager = null;
     private SellerManager(){    }
@@ -11,6 +16,14 @@ public class SellerManager {
     }
 
     AccountManager accountManager = AccountManager.getInstance();
+    CSCLManager csclManager = CSCLManager.getInstance();
+
+
+    public Company viewCompanyInformation(String username) throws UserNotAvailableException {
+        Seller seller = (Seller) accountManager.getUserByUsername(username);
+        return seller.getCompany();
+    }
+
 
 
 
