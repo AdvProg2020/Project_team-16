74a57abf76fec9e579bd06d14eea36ba26781912
ckoorosh <53394330+ckoorosh@users.noreply.github.com>
2020-05-01 10:05:32 +0430

    Company info test implemented

--- src/test/java/ModelPackage/System/SellerManagerTest.java
+++ src/test/java/ModelPackage/System/SellerManagerTest.java
@@ -1,8 +1,52 @@
 package ModelPackage.System;
 
+import ModelPackage.Product.Company;
+import ModelPackage.System.exeption.account.UserNotAvailableException;
+import ModelPackage.Users.Cart;
 import ModelPackage.Users.Seller;
+import ModelPackage.Users.User;
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.Mock;
+import mockit.MockUp;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class SellerManagerTest {
     SellerManager sellerManager;
     Seller marmof;
+    Company adidas;
+
+    {
+        adidas = new Company("Adidas","115", "Clothing",new ArrayList<>());
+        sellerManager = SellerManager.getInstance();
+        marmof = new Seller(
+                "marmofayezi",
+                "marmof.ir",
+                "Cyrus",
+                "Statham",
+                "marmof@gmail.com",
+                "+1 992 1122",
+                new Cart(),
+                adidas,
+                10000000
+        );
+
+    }
+
+
+    @Test
+    public void viewCompanyInformation() throws UserNotAvailableException {
+        new MockUp<AccountManager>(){
+            @Mock
+            User getUserByUsername(String username){
+                return marmof;
+            }
+        };
+
+        Company actual = sellerManager.viewCompanyInformation("marmofayezi");
+        Assert.assertEquals(adidas,actual);
+    }
 }
