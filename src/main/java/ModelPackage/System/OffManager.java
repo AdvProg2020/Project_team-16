405f21e9cf8af5a31d61c1c59a0a52407d636a9b
Ali Hatami <hatam008@users.noreply.github.com>
2020-05-13 01:45:51 +0430

    delete off

--- src/main/java/ModelPackage/System/OffManager.java
+++ src/main/java/ModelPackage/System/OffManager.java
@@ -104,5 +104,8 @@ public class OffManager {
         RequestManager.getInstance().addRequest(request);
     }
 
-
+    public void delteOff(int id) throws NoSuchAOffException {
+        Off off = findOffById(id);
+        DBManager.delete(off);
+    }
 }
