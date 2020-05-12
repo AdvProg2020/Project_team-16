d9ecfb0e740cc9c0eb0b1090fc3bb3f55868f5f5
Ali Hatami <hatam008@users.noreply.github.com>
2020-05-13 01:44:45 +0430

    Edit Time Of Off

--- src/main/java/ModelPackage/System/OffManager.java
+++ src/main/java/ModelPackage/System/OffManager.java
@@ -36,5 +36,35 @@ public class OffManager {
         RequestManager.getInstance().addRequest(request);
     }
 
+    Off findOffById(int id) throws NoSuchAOffException {
+        Off off = DBManager.load(Off.class,id);
+        if (off == null) throw new NoSuchAOffException(id);
+        return off;
+    }
+
+    public void EditStartTimeOfOff(User editor, int id, Date newDate)
+            throws NoSuchAOffException {
+        findOffById(id);
+        OffChangeAttributes edit = new OffChangeAttributes();
+        edit.setStart(newDate);
+        edit.setTarget(id);
+        DBManager.save(edit);
+        String strRequest = String.format("%s requested to change start time of off (%d) to %s",editor.getUsername(),id,newDate);
+        Request request = new Request(editor.getUsername(),RequestType.EDIT_OFF,strRequest,edit);
+        RequestManager.getInstance().addRequest(request);
+    }
+
+    public void EditEndTimeOfOff(User editor, int id, Date newDate)
+            throws NoSuchAOffException {
+        findOffById(id);
+        OffChangeAttributes edit = new OffChangeAttributes();
+        edit.setEnd(newDate);
+        edit.setTarget(id);
+        DBManager.save(edit);
+        String strRequest = String.format("%s requested to change end time of off (%d) to %s",editor.getUsername(),id,newDate);
+        Request request = new Request(editor.getUsername(),RequestType.EDIT_OFF,strRequest,edit);
+        RequestManager.getInstance().addRequest(request);
+    }
+
 
 }
