9b2283a124e6336859cfc39d7420622d93609c79
Ali Hatami <hatam008@users.noreply.github.com>
2020-05-13 00:41:52 +0430

    creating offManager

---
+++ src/main/java/ModelPackage/System/OffManager.java
@@ -0,0 +1,11 @@
+package ModelPackage.System;
+
+public class OffManager {
+    private static OffManager offManager;
+
+    public static OffManager getInstance() {
+        return offManager;
+    }
+
+
+}
