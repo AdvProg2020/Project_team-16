8fb2ab66d76c9b9274f4235bf6b0494d62ad5a95
sajad-paksima <59170401+sajad-paksima@users.noreply.github.com>
2020-05-14 13:24:15 +0430

    method createDiscount created

--- src/main/java/controler/ManagerController.java
+++ src/main/java/controler/ManagerController.java
@@ -1,16 +1,22 @@
 package controler;
 
-import ModelPackage.System.AccountManager;
-import ModelPackage.System.database.DBManager;
-import ModelPackage.Users.User;
+import ModelPackage.System.DiscountManager;
+import ModelPackage.System.exeption.discount.NotValidPercentageException;
+import ModelPackage.System.exeption.discount.StartingDateIsAfterEndingDate;
 
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ManagerController {
-    private AccountManager accountManager = AccountManager.getInstance();
 
-    public List<User> manageUsers (){
-       return DBManager.loadAllData(User.class);
+    public void createDiscount(String[] data) throws ParseException,
+            NotValidPercentageException, StartingDateIsAfterEndingDate {
+        Date startTime = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse(data[0]);
+        Date endTime = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse(data[1]);
+        int offPercentage = Integer.parseInt(data[2]);
+        long maxDiscount = Long.parseLong(data[3]);
+        DiscountManager.getInstance().createDiscountCode(startTime, endTime, offPercentage, maxDiscount);
     }
 
 
