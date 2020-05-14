c0879d1181f55150d3e7240622543a4a96ae1521
ckoorosh <53394330+ckoorosh@users.noreply.github.com>
2020-05-14 13:31:53 +0430

    discount create

--- src/main/java/controler/ManagerController.java
+++ src/main/java/controler/ManagerController.java
@@ -1,7 +1,5 @@
 package controler;
 
-import ModelPackage.System.AccountManager;
-import ModelPackage.System.DiscountManager;
 import ModelPackage.System.database.DBManager;
 import ModelPackage.System.exeption.discount.NotValidPercentageException;
 import ModelPackage.System.exeption.discount.StartingDateIsAfterEndingDate;
@@ -12,11 +10,18 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-public class ManagerController {
-    private AccountManager accountManager = AccountManager.getInstance();
+public class ManagerController extends Controller {
 
     public List<User> manageUsers () {
         return DBManager.loadAllData(User.class);
+    }
+
+    public User viewUser(String username){
+        return accountManager.getUserByUsername(username);
+    }
+
+    public void deleteUser(String username){
+        managerManager.deleteUser(username);
     }
 
     public void createDiscount(String[] data) throws ParseException,
@@ -25,6 +30,6 @@ public class ManagerController {
         Date endTime = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse(data[1]);
         int offPercentage = Integer.parseInt(data[2]);
         long maxDiscount = Long.parseLong(data[3]);
-        DiscountManager.getInstance().createDiscountCode(startTime, endTime, offPercentage, maxDiscount);
+        discountManager.createDiscountCode(startTime, endTime, offPercentage, maxDiscount);
     }
 }
