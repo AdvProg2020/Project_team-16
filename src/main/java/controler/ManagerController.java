4e27cd9f9714a33bf1dcedce8398008acc577538
sajad-paksima <59170401+sajad-paksima@users.noreply.github.com>
2020-05-14 13:27:23 +0430

    one method manageUsers fixed

--- src/main/java/controler/ManagerController.java
+++ src/main/java/controler/ManagerController.java
@@ -1,14 +1,23 @@
 package controler;
 
+import ModelPackage.System.AccountManager;
 import ModelPackage.System.DiscountManager;
+import ModelPackage.System.database.DBManager;
 import ModelPackage.System.exeption.discount.NotValidPercentageException;
 import ModelPackage.System.exeption.discount.StartingDateIsAfterEndingDate;
+import ModelPackage.Users.User;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class ManagerController {
+    private AccountManager accountManager = AccountManager.getInstance();
+
+    public List<User> manageUsers () {
+        return DBManager.loadAllData(User.class);
+    }
 
     public void createDiscount(String[] data) throws ParseException,
             NotValidPercentageException, StartingDateIsAfterEndingDate {
@@ -18,6 +27,4 @@ public class ManagerController {
         long maxDiscount = Long.parseLong(data[3]);
         DiscountManager.getInstance().createDiscountCode(startTime, endTime, offPercentage, maxDiscount);
     }
-
-
 }
