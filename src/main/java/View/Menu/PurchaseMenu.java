7a7ead59ea7c59c9d615ea3ec169b257327db611
Ali Hatami <hatam008@users.noreply.github.com>
2020-05-15 02:51:53 +0430

    purchase menu (just structure)

--- src/main/java/View/Menu/PurchaseMenu.java
+++ src/main/java/View/Menu/PurchaseMenu.java
@@ -1,7 +1,30 @@
 package View.Menu;
 
+import View.exceptions.InvalidCommandException;
+import View.exceptions.NotAnAvailableMenu;
+
 public class PurchaseMenu extends Menu {
     public PurchaseMenu(Menu parent) {
         super("Purchase Menu",parent);
+    }
+
+    @Override
+    void helpPrinter() {
+        /*TODO*/
+    }
+
+    @Override
+    void executeValidCommand(String command) throws InvalidCommandException {
+        /*TODO*/
+    }
+
+    @Override
+    void goToSubMenusIfAvailable(String menuName) throws NotAnAvailableMenu {
+        /*TODO*/
+    }
+
+    @Override
+    void additionalPrints() {
+        /*TODO*/
     }
 }
