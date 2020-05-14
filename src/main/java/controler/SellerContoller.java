f4c622aabe70bbbf274a08fffe340c57d8aad4a4
sajad-paksima <59170401+sajad-paksima@users.noreply.github.com>
2020-05-14 17:45:23 +0430

    method addOff created

--- src/main/java/controler/SellerContoller.java
+++ src/main/java/controler/SellerContoller.java
@@ -6,11 +6,14 @@ import ModelPackage.Product.Company;
 import ModelPackage.Product.Product;
 import ModelPackage.System.exeption.category.NoSuchACategoryException;
 import ModelPackage.System.exeption.category.NoSuchAProductInCategoryException;
+import ModelPackage.System.exeption.off.InvalidTimes;
 import ModelPackage.System.exeption.off.NoSuchAOffException;
 import ModelPackage.System.exeption.product.NoSuchAProductException;
 import ModelPackage.Users.Seller;
 import View.PrintModels.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class SellerContoller extends Controller{
@@ -82,6 +85,17 @@ public class SellerContoller extends Controller{
                 off.getStartTime(),
                 off.getEndTime(),
                 off.getOffPercentage());
+    }
+
+    public void addOff(String[] data, String sellerUserName) throws ParseException, InvalidTimes {
+        Seller seller = (Seller) accountManager.getUserByUsername(sellerUserName);
+        Date startTime = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse(data[0]);
+        Date endTime = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss").parse(data[1]);
+        Date[] dates = new Date[2];
+        dates[0] = startTime;
+        dates[1] = endTime;
+        int offPercentage = Integer.parseInt(data[2]);
+        offManager.createOff(seller, dates, offPercentage);
     }
 
     private ArrayList<Integer> addProductIdsToOffPM(Off off) {
