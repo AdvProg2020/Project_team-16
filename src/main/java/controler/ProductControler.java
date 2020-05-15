fa1bcb4a5a0b935410ba0d4d3b8e4c29ba666e03
sajad-paksima <59170401+sajad-paksima@users.noreply.github.com>
2020-05-16 01:01:08 +0430

    method assignComment created

--- src/main/java/controler/ProductControler.java
+++ src/main/java/controler/ProductControler.java
@@ -1,5 +1,21 @@
 package controler;
 
-public class ProductControler {
+import ModelPackage.Product.Comment;
+import ModelPackage.Product.CommentStatus;
+import ModelPackage.System.exeption.product.NoSuchAProductException;
+
+public class ProductControler extends Controller{
+
+    public void assignComment(String[] data) throws NoSuchAProductException {
+        String userId = data[0];
+        String commentTitle = data[1];
+        String commentText = data[2];
+        int productId = Integer.parseInt(data[3]);
+        // TODO : check if user bought this product
+        Comment comment = new Comment(userId, commentTitle, commentText,
+                CommentStatus.NOT_VERIFIED, false);
+        comment.setProduct(productManager.findProductById(productId));
+        csclManager.createComment(comment);
+    }
 
 }
