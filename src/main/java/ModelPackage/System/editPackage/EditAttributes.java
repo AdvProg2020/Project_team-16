da52929080bab5f113ae4135987df925e62ce590
Ali Hatami <hatam008@users.noreply.github.com>
2020-05-16 01:08:02 +0430

    product edit changed

--- src/main/java/ModelPackage/System/editPackage/EditAttributes.java
+++ src/main/java/ModelPackage/System/editPackage/EditAttributes.java
@@ -1,7 +1,10 @@
 package ModelPackage.System.editPackage;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name = "t_edit_attributes")
 @Inheritance(strategy = InheritanceType.JOINED)
