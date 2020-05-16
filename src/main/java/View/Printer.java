8ac9d99c17e2722b9fca69e4302bfc18d701f320
ckoorosh <53394330+ckoorosh@users.noreply.github.com>
2020-05-17 02:27:55 +0430

    Printer prototypes

--- src/main/java/View/Printer.java
+++ src/main/java/View/Printer.java
@@ -1,5 +1,9 @@
 package View;
 
+import View.PrintModels.*;
+
+import java.util.List;
+
 public class Printer {
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
@@ -14,4 +18,26 @@ public class Printer {
     public static void printMessage(String message){
         System.out.println(message);
     }
+
+
+        public void productPrinterShort(MiniProductPM product){}
+        public void productPrintFull(FullProductPM product){}
+        public void usersPrinter(List<UserMiniPM> users){}
+        public void userPrinter(UserFullPM user){}
+        public void printAllProducts(List<MiniProductPM> products){}
+        public void printAllDiscountCodes(List<DiscountMiniPM> discountCode){}
+        public void printDiscountManager(DisCodeManagerPM discountCode){}
+        public void printAllRequests(List<RequestPM> requests){}
+        public void printDetailedRequest(RequestPM request){}
+        public void printAllCategories(List<CategoryPM> categories){}
+        public void printCompany(CompanyPM company){}
+        public void printSaleHistory(List<SellLogPM> sellLog){}
+        public void viewOffs(List<OffPM> offs){}
+        public void viewBalance(long balance){}
+        public void viewCart(CartPM cartPM){}
+        public void viewProductsInCart(InCartPM cart){}
+        public void viewOrderHistory (List<OrderLogPM> orders){}
+        public void viewUserDiscounts(DisCodeUserPM discountCode){}
+        public void comparePrinter(FullProductPM[] products){}
+        public void printComments(List<CommentPM> comments){}
 }
