e97be7a51c1634a3769abc4b777d82e158602d57
sajad-paksima <59170401+sajad-paksima@users.noreply.github.com>
2020-05-16 16:29:55 +0430

    Merge branch 'master' of https://github.com/AdvProg2020/Project_team-16

--- src/main/java/controler/AccountController.java
+++ src/main/java/controler/AccountController.java
@@ -1,6 +1,10 @@
 package controler;
 
+import ModelPackage.Product.Product;
 import ModelPackage.System.exeption.account.UserNotAvailableException;
+import ModelPackage.Users.Cart;
+import ModelPackage.Users.Customer;
+import ModelPackage.Users.SubCart;
 import ModelPackage.Users.User;
 import View.PrintModels.UserFullPM;
 
@@ -15,12 +19,19 @@ public class AccountController extends Controller {
         }
     }
 
-    public void createAccount(String[] info, String type){
+    public void createAccount(String[] info, String type, Cart cart){
         accountManager.createAccount(info, type);
+        if (type.equalsIgnoreCase("customer")){
+            addCartToCustomer(info[0], cart);
+        }
     }
 
-    public String login(String username, String password) {
-        return accountManager.login(username, password);
+    public String login(String username, String password, Cart cart) {
+        String roll = accountManager.login(username, password);
+        if (roll.equals("ModelPackage.Users.Customer")){
+            addCartToCustomer(username, cart);
+        }
+        return roll;
     }
 
     public UserFullPM viewPersonalInfo(String username){
@@ -46,5 +57,28 @@ public class AccountController extends Controller {
 
     public void logout(String username){
         accountManager.logout(username);
+    }
+
+    private void addCartToCustomer(String username, Cart cart){
+        Customer customer = (Customer)accountManager.getUserByUsername(username);
+        Cart previousCart = customer.getCart();
+
+        boolean added = false;
+        for (SubCart subCart : cart.getSubCarts()) {
+            added = false;
+            Product product = subCart.getProduct();
+            for (SubCart previousCartSubCart : previousCart.getSubCarts()) {
+                Product previousCartProduct = previousCartSubCart.getProduct();
+                if (previousCartProduct.getId() == product.getId() &&
+                        previousCartSubCart.getSeller().getUsername().equals(subCart.getSeller().getUsername())){
+                    previousCartSubCart.setAmount(previousCartSubCart.getAmount() + subCart.getAmount());
+                    added = true;
+                    break;
+                }
+            }
+            if (!added){
+                previousCart.getSubCarts().add(subCart);
+            }
+        }
     }
 }
