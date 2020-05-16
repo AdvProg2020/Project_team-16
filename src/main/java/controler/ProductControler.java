c3a209839f43f95987daade7d9ea45307b572e8a
sajad-paksima <59170401+sajad-paksima@users.noreply.github.com>
2020-05-16 16:22:30 +0430

    method sort created

--- src/main/java/controler/ProductControler.java
+++ src/main/java/controler/ProductControler.java
@@ -4,6 +4,7 @@ import ModelPackage.Maps.SellerIntegerMap;
 import ModelPackage.Product.Comment;
 import ModelPackage.Product.CommentStatus;
 import ModelPackage.Product.Product;
+import ModelPackage.System.SortType;
 import ModelPackage.System.exeption.account.ProductNotHaveSellerException;
 import ModelPackage.System.exeption.product.NoSuchAProductException;
 import ModelPackage.Users.Seller;
@@ -11,6 +12,7 @@ import ModelPackage.Users.User;
 import View.PrintModels.CommentPM;
 import View.PrintModels.FullProductPM;
 import View.PrintModels.MiniProductPM;
+import controler.exceptions.NotAvailableSort;
 import controler.exceptions.ProductsNotBelongToUniqueCategoryException;
 
 import java.util.ArrayList;
@@ -96,6 +98,45 @@ public class ProductControler extends Controller{
         availableSort[5] = "Bought Amount";
         availableSort[6] = "Score";
         return availableSort;
+    }
+
+    public void sortProducts(String[] data) throws NotAvailableSort, NoSuchAProductException {
+        String sortName = data[0];
+        checkIfSortIsAvailable(sortName);
+        ArrayList<Product> toSortProducts = new ArrayList<>();
+        for (int i = 1; i < data.length; i++) {
+            toSortProducts.add(productManager.findProductById(Integer.parseInt(data[i])));
+        }
+        SortType sortType = findSortType(data[0]);
+        sortManager.sort(toSortProducts, sortType);
+    }
+
+    private SortType findSortType(String sortType) {
+        switch (sortType) {
+            case "Name" :
+                return SortType.NAME;
+            case "Time" :
+                return SortType.TIME;
+            case "View" :
+                return SortType.VIEW;
+            case "More Price" :
+                return SortType.MORE_PRICE;
+            case "Less Price" :
+                return SortType.LESS_PRICE;
+            case "Bought Amount" :
+                return SortType.BOUGHT_AMOUNT;
+            case "Score" :
+                return SortType.SCORE;
+        }
+        return null;
+    }
+
+    private void checkIfSortIsAvailable(String sortName) throws NotAvailableSort {
+        String[] availableSorts = showAvailableSorts();
+        for (String sort : availableSorts) {
+            if (!sort.equals(sortName))
+                throw new NotAvailableSort(sortName);
+        }
     }
 
     private void checkIfTwoProductsDoesNotBelongToUniqueCategory(int firstProductId, int secondProductId)
