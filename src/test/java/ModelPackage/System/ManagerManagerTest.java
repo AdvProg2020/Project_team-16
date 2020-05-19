5d7f775c16c2b5139807f1aee2de2269aa35b39a
ckoorosh <53394330+ckoorosh@users.noreply.github.com>
2020-05-19 13:15:56 +0430

    ManagerManagerTest fixed

--- src/test/java/ModelPackage/System/ManagerManagerTest.java
+++ src/test/java/ModelPackage/System/ManagerManagerTest.java
@@ -1,24 +1,31 @@
 package ModelPackage.System;
 
+import ModelPackage.System.database.DBManager;
+import ModelPackage.System.exeption.account.SecondManagerByUserException;
 import ModelPackage.System.exeption.account.UserNotAvailableException;
 import ModelPackage.Users.Cart;
 import ModelPackage.Users.Manager;
 import ModelPackage.Users.User;
+import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ManagerManagerTest {
     private ManagerManager managerManager;
-    private List<Manager> managers;
+    private ArrayList<Manager> managers;
+    private ArrayList<User> users;
     private Manager hatam;
     private User marmof;
 
     {
         managerManager = ManagerManager.getInstance();
-        managers = managerManager.getManagers();
 
         hatam = new Manager(
                 "hatam008",
@@ -39,7 +46,29 @@ public class ManagerManagerTest {
                 new Cart()
         );
 
-        AccountManager.getInstance().getUsers().add(marmof);
+        managers = new ArrayList<>();
+        managers.add(hatam);
+
+        users = new ArrayList<>();
+        users.add(marmof);
+        users.add(hatam);
+    }
+
+    @Before
+    public void initialize(){
+        new MockUp<DBManager>(){
+            @Mock
+            public void delete(Object o) {
+                users.remove(marmof);
+            }
+            @Mock
+            public <T> T load(Class<T> type, Serializable username) throws UserNotAvailableException {
+                if (username.equals("marmofayezi")) return (T) marmof;
+                else if (username.equals("hatam008") && type.getName().equals("ModelPackage.Users.Manager")) return (T) hatam;
+                else return null;
+            }
+        };
+
     }
 
     @Test
@@ -59,10 +88,40 @@ public class ManagerManagerTest {
 
     @Test
     public void deleteUser() {
+        new MockUp<DBManager>(){
+          @Mock
+          public <T> List<T> loadAllData(Class<T> type){
+              return (List<T>) users;
+          }
+        };
         managerManager.deleteUser("marmofayezi");
-        ArrayList<User> expected = new ArrayList<>();
-        ArrayList<User> actual = (ArrayList<User>) AccountManager.getInstance().getUsers();
-        Assert.assertArrayEquals(new ArrayList[]{expected}, new ArrayList[]{actual});
+        List<User> expected = users;
+        List<User> actual = DBManager.loadAllData(User.class);
+        Assert.assertArrayEquals(expected.toArray(), actual.toArray());
     }
 
+    @Test
+    public void checkFirstManager() {
+        new MockUp<DBManager>(){
+            @Mock
+            public <T> List<T> loadAllData(Class<T> type){
+                List<Manager> allManagers = new ArrayList<>();
+                return (List<T>) allManagers;
+            }
+        };
+
+        managerManager.checkIfIsTheFirstManager();
+    }
+
+    @Test(expected = SecondManagerByUserException.class)
+    public void checkFirstManager_SecondManager() {
+        new MockUp<DBManager>(){
+            @Mock
+            public <T> List<T> loadAllData(Class<T> type){
+                return (List<T>) managers;
+            }
+        };
+
+        managerManager.checkIfIsTheFirstManager();
+    }
 }
