e2f379b765f4052357c85c2433b4bb41e23bb1cf
ckoorosh <53394330+ckoorosh@users.noreply.github.com>
2020-05-19 13:26:34 +0430

    create account and login test in Controller

--- src/test/java/controller/AccountControllerTest.java
+++ src/test/java/controller/AccountControllerTest.java
@@ -2,17 +2,21 @@ package controller;
 
 import ModelPackage.Product.Company;
 import ModelPackage.System.AccountManager;
+import ModelPackage.System.exeption.account.NotVerifiedSeller;
 import ModelPackage.System.exeption.account.UserNotAvailableException;
 import ModelPackage.Users.Cart;
 import ModelPackage.Users.Manager;
 import ModelPackage.Users.Seller;
 import ModelPackage.Users.User;
 import controler.AccountController;
+import mockit.Expectations;
 import mockit.Mock;
 import mockit.MockUp;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AccountControllerTest {
     AccountController accountController = new AccountController();
@@ -21,6 +25,8 @@ public class AccountControllerTest {
     private Manager hatam;
     private Seller sapa;
     private Company adidas;
+
+    ArrayList<User> users;
 
     {
         marmof = new User("marmofayezi",
@@ -44,16 +50,18 @@ public class AccountControllerTest {
         adidas = new Company("Adidas", "115", "Clothing", new ArrayList<>());
 
         sapa = new Seller(
-                "marmofayezi",
-                "marmof.ir",
-                "Cyrus",
-                "Statham",
-                "marmof@gmail.com",
-                "+1 992 1122",
+                "sapa",
+                "sapa.pak",
+                "Sajad",
+                "Paksima",
+                "sapa@gmail.com",
+                "+2 942 6722",
                 new Cart(),
                 adidas,
-                10000000
+                0
         );
+
+        users = new ArrayList<>();
     }
 
     @Test
@@ -78,5 +86,40 @@ public class AccountControllerTest {
         accountController.usernameInitialCheck("marmof");
     }
 
+    @Test
+    public void createAccount() throws UserNotAvailableException {
+        new MockUp<AccountManager>(){
+            @Mock
+            public void createAccount(String[] info, String type){
+                users.add(marmof);
+            }
+        };
+        String[] info = {
+                "marmofayezi",
+                "marmof.ir",
+                "Mohamadreza",
+                "Mofayezi",
+                "marmof@gmail.com",
+                "09121232222",
+                "1000000"
+        };
 
+        accountController.createAccount(info, "customer");
+
+        Assert.assertTrue(users.contains(marmof));
+    }
+
+    @Test
+    public void login() throws NotVerifiedSeller, UserNotAvailableException {
+        new MockUp<AccountManager>(){
+            @Mock
+            public String login(String username, String password){
+                return "Seller";
+            }
+        };
+
+        String actual =  accountController.login("sapa", "sapa.pak");
+
+        Assert.assertEquals("Seller", actual);
+    }
 }
