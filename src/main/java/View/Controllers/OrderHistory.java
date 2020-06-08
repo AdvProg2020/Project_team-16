4e0ff601b5838986acd650615374db109b64fc15
Ali Hatami <hatam008@users.noreply.github.com>
2020-06-08 15:54:05 +0430

    some of FXMLs

---
+++ src/main/java/View/Controllers/OrderHistory.java
@@ -0,0 +1,23 @@
+package View.Controllers;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+
+public class OrderHistory {
+    public JFXButton back;
+    public JFXButton cartButt;
+    public JFXButton minimize;
+    public JFXButton close;
+    public TableColumn orderNno;
+    public TableColumn dateColumn;
+    public Label no;
+    public Label date;
+    public Label price;
+    public Label discount;
+    public Button viewProduct;
+    public Label delStatus;
+
+
+}
---
+++ src/main/java/View/Controllers/ProductsPage.java
@@ -0,0 +1,26 @@
+package View.Controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXSlider;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
+
+public class ProductsPage {
+    public JFXButton back;
+    public JFXButton cartButt;
+    public JFXButton minimize;
+    public JFXButton close;
+    public VBox filterVBox;
+    public JFXSlider minPrice;
+    public JFXSlider maxPrice;
+    public ToggleGroup ADOrder;
+    public ToggleGroup type;
+    public ScrollPane productVBox;
+    public TextField serch;
+    public Button searchBut;
+    public Button ignoreSearch;
+
+}
