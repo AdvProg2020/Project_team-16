61edba3a218772782ccdb3b3266eb03abd89fe66
Ali Hatami <hatam008@users.noreply.github.com>
2020-06-11 22:12:56 +0430

    move FXMLs to main resource folder. previous causes problem loading images and CSSs.

--- src/main/java/View/Main.java
+++ src/main/java/View/Main.java
@@ -70,7 +70,7 @@ public class Main extends Application {
     }
 
     public static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getClassLoader().getResource("/fxmls/"+ fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getClassLoader().getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
 }
