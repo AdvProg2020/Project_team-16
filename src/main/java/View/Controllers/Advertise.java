8633529b84633ce9492a62a002e57a65cbab92d7
Ali Hatami <hatam008@users.noreply.github.com>
2020-06-23 15:59:26 +0430

    Deleting helper button

--- src/main/java/View/Controllers/Advertise.java
+++ src/main/java/View/Controllers/Advertise.java
@@ -7,7 +7,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.ImagePattern;
@@ -21,8 +20,6 @@ import java.util.Random;
 import static View.Controllers.Content.COLORS;
 
 public class Advertise {
-    @FXML
-    private Button button;
     @FXML
     private Circle image;
     @FXML
